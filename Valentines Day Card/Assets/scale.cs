524b6f58f3ef6b46b67aa06549773f7991d7b635
michaelbridgette <c00205948@itcarlow.ie>
2018-08-24 17:08:46 +0100

    Card Done

---
+++ Valentines Day Card/Assets/scale.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scale : MonoBehaviour {
+    float timer;
+    Vector3 vector;
+	// Use this for initialization
+	void Start () {
+        vector = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += 1.0f/60.0f;
+
+        if (timer < 0.1)
+        {
+            transform.localScale += new Vector3(0.0002f, 0.0002f, 0.0002f);
+        }
+        else if (timer >0.1 && timer <0.2)
+        {
+            transform.localScale -= new Vector3(0.0002f, 0.0002f, 0.0002f);
+            
+        }
+
+        if(transform.localScale.x <= vector.x)
+        {
+            timer = 0;
+        }
+
+    }
+}
